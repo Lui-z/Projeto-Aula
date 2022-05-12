3c18c7dc3c131d4e812610df611dafdcab208421
Luiz Eduardo  Fernandes <79099954+Lui-z@users.noreply.github.com>
2022-05-12 13:40:05 -0300

    aula

--- livraria/js/login.js
+++ livraria/js/login.js
@@ -1,13 +1,13 @@
+//atribuindo o acesso ao formcontato para a variavel formC
+var formL=document.forms.formLogin
 //Evento de clique no botão confirmar
-var formL=document.forms.formlogin
-//acesso ao formcontato para variavel formC
-document.querySelector("#btlogin").onclick=function(){
-    //alert('Aprende a escrever direito')
-    if(formL.email.value == ""){
-        alert("Preencha o email!")
-    }else if (formL.senha.value == ""){
-        alert("Preencha a senha!")
-    }else{
-        formL.submit()
-    }
+document.querySelector("#btLogin").onclick=function(){
+   // alert('ok')
+   if(formL.email.value == ""){
+       alert("Preencha o email!")
+   }else if (formL.senha.value == ""){
+   alert("Preencha o senha!")
+   }else{ 
+    formL.submit()
+ }
 }
